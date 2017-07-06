21eff470495391ef5de02c31f904734172eda272
ubuntu32 <w.adachi1127@i.softbank.jp>
2017-07-07 00:53:12 +0900

    更新　ハンバーガーjquery作ったけど横スライド試したいので保留

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   $('#openMenu').children('a').on('click', function(e){
     e.preventDefault();
-    console.log("click");
-  });
+    //console.log("click");
+    $('nav').height( $(document).height() ); // 高さいっぱいに
+    var width = $(document).width();
+    console.log(width);
+
+});
 });
