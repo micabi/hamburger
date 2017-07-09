71823fc3a1f5c6def0b42bdb2677954661af13a5
ubuntu32 <w.adachi1127@i.softbank.jp>
2017-07-10 00:27:06 +0900

    更新　ヘッダー固定、右からメニュー、ボディーがスライド

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
-  $('#openMenu').children('a').on('click', function(e){
-    e.preventDefault();
-    //console.log("click");
-    $('nav').height( $(document).height() ); // 高さいっぱいに
-    var width = $(document).width();
-    console.log(width);
+  var toggleBtn = $('#openMenu'); // ボタン
+  var closeBtn = $('#close-btn');
+  var gMenu = $('#global-menu'); // グローバルメニュー
+  var body = $(document.body); // ボディ
+  var menuWidth = gMenu.outerWidth(); // メニュー幅
+  var layer = $('#layer'); // メニューを出したときのバックグラウンドレイヤー
 
-});
+  toggleBtn.children('a').on('click', function(e){
+    e.preventDefault();
+    body.toggleClass('open');
+
+    if (body.hasClass('open')) {
+      body.animate({'right': menuWidth}, 300);
+      gMenu.animate({'right': 0}, 300);
+      layer.show();
+    }else{
+      body.animate({'right': 0}, 300);
+      gMenu.animate({'right': -menuWidth}, 300);
+      layer.hide();
+    }
+  });
+
+  layer.on('click', function(){
+    body.animate({'right': 0}, 300);
+    gMenu.animate({'right': -menuWidth}, 300);
+    layer.hide();
+    body.removeClass('open');
+  });
+
+  closeBtn.on('click', function(){
+    body.animate({'right': 0}, 300);
+    gMenu.animate({'right': -menuWidth}, 300);
+    layer.hide();
+    body.removeClass('open');
+  });
 });
