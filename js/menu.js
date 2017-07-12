8c88c1305164dc0ea3ff554e3fcb2d5232562f3e
ubuntu32 <w.adachi1127@i.softbank.jp>
2017-07-12 19:25:40 +0900

    修正　iPhoneでメニューが一部表示されないのと、閉じたときヘッダーが戻らないのを直す。
    - style.css overflow-xを.layerではなくbodyにつけた。
    - menu.js ヘッダーもposition: fixed;なのでbodyのanimate()と同じ動きをするようにした。

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   var toggleBtn = $('#openMenu'); // ボタン
   var closeBtn = $('#close-btn');
+  var header = $('header');
   var gMenu = $('#global-menu'); // グローバルメニュー
   var body = $(document.body); // ボディ
   var menuWidth = gMenu.outerWidth(); // メニュー幅
@@ -13,11 +14,13 @@ $(document).ready(function(){
     if (body.hasClass('open')) {
       body.animate({'right': menuWidth}, 300);
       gMenu.animate({'right': 0}, 300);
+      header.animate({'right': menuWidth}, 300); // headerも固定しているのでbodyと同じ動きにする
       toggleBtn.hide(); // ハンバーガーボタン非表示
       layer.show();
     }else{
       body.animate({'right': 0}, 300);
       gMenu.animate({'right': -menuWidth}, 300);
+      header.animate({'right': 0}, 300);
       toggleBtn.show(); // ハンバーガーボタン表示
       layer.hide();
     }
@@ -26,6 +29,7 @@ $(document).ready(function(){
   layer.on('click', function(){
     body.animate({'right': 0}, 300);
     gMenu.animate({'right': -menuWidth}, 300);
+    header.animate({'right': 0}, 300);
     layer.hide();
     toggleBtn.show(); // ハンバーガーボタン表示
     body.removeClass('open');
@@ -34,6 +38,7 @@ $(document).ready(function(){
   closeBtn.on('click', function(){
     body.animate({'right': 0}, 300);
     gMenu.animate({'right': -menuWidth}, 300);
+    header.animate({'right': 0}, 300);
     layer.hide();
     toggleBtn.show(); // ハンバーガーボタン表示
     body.removeClass('open');
